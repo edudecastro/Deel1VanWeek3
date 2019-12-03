86f5f5c80ea7b906d4418b9651615b8e83049f5d
edoca <edocasimiro@hotmail.com>
2019-12-03 15:51:20 +0100

    Add project files.

---
+++ Deel1VanWeek3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Deel1VanWeek3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+
+
+
+    }
+
+    public class Question
+    {
+
+        private String 
+      
+
+    }
+}
